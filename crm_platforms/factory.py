bb15abc09edbc894f39ecdf35c117767e7eb91a6
sohamganatra <sohamganatra1@gmail.com>
2023-03-05 22:25:45 +0530

    Added hubspot file

--- crm_platforms/factory.py
+++ crm_platforms/factory.py
@@ -1,7 +1,10 @@
 from crm_platforms.pipedrive import Pipedrive
+from crm_platforms.hubspot import Hubspot
 
 def create_platform(platform_type, auth):
     if platform_type == "pipedrive":
         return Pipedrive("pipedrive",auth)
+    if platform_type == "hubspot":
+        return Hubspot("hubspot",auth)
     else:
         return None
